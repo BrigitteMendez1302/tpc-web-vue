9f560ba71e143db3efc7f7609e1e0afea328e3ca
SirJoc <josiasolaya2016@gmail.com>
2021-06-15 13:03:36 -0500

    feat: added component lessons

--- src/components/lessons/lessons.vue
+++ src/components/lessons/lessons.vue
@@ -1,68 +1,59 @@
 <template>
   <v-card>
-    <v-card><h1 class="text-lg-left">Mis reservas</h1></v-card>
+    <v-card class="pa-md-5 mx-lg-14"
+            color="indigo lighten-5"
+            width="1080px">
+      <v-row><v-card-title><h2> Mis reservas </h2></v-card-title></v-row></v-card>
     <v-card-title>
       <v-spacer></v-spacer>
-      <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-          single-line
-          hide-details
-      ></v-text-field>
     </v-card-title>
-    <v-card-text>
-      <v-data-table :headers="headers"
-                    :items="displayLessons"
-                    :items-per-page="5"
-                    :search="search"
-                    class="elevation-1" ref="lessonsTable">
-        <template v-slot:top>
-          <v-dialog v-model="dialog" max-width="500px">
-            <v-card>
-              <v-card-title>
-                <span class="headline">{{ formTitle }}</span>
-              </v-card-title>
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.id" label="Id"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.tutorId" label="Tutor"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.lessonType" label="Type"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.courseId" label="Course"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.vacants" label="Vacants"></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-            </v-card>
-          </v-dialog>
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="headline">Delete Item</v-card-title>
-              <v-card-text>
-                <p>Are you sure you want to delete the item <b>{{ editedItem.id }}</b></p>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-2" text @click="closeDelete">Cancel</v-btn>
-                <v-btn color="blue darken-2" text @click="deleteItemConfirm">OK</v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </template>
-      </v-data-table>
-    </v-card-text>
+    <v-list subheader>
+      <v-subheader class="subtitle-1" >Lunes 14 de Junio</v-subheader>
+
+      <v-list-item class=" ma-3 content"
+                   v-for="les in displayLessons"
+                   :key="les"
+      >
+        <v-list-item-content>
+          <v-col cols="3">
+            <v-list-item-title class="m-list-item-title">{{les.id}}</v-list-item-title>
+          </v-col>
+        </v-list-item-content>
+
+        <v-list-item-content>
+          <v-col cols="7">
+            <v-list-item-title class="m-list-item-title" >{{les.tutorId}}</v-list-item-title>
+          </v-col>
+        </v-list-item-content>
+
+        <v-list-item-content>
+          <v-col cols="7">
+            <v-list-item-title class="m-list-item-title" >{{les.lessonType}}</v-list-item-title>
+          </v-col>
+        </v-list-item-content>
+
+        <v-list-item-content>
+          <v-col cols="10"
+                 align-self="justify">
+            <v-list-item-title class="m-list-item-title" >{{les.courseId}}</v-list-item-title>
+          </v-col>
+        </v-list-item-content>
+        <v-list-item-content>
+          <v-col cols="3">
+            <v-list-item-title class="m-list-item-title" >{{les.vacants}}</v-list-item-title>
+          </v-col>
+        </v-list-item-content>
+        <!--        v-text="chat.title"-->
+        <v-list-item-content>
+          <v-col cols="5">
+            <div>
+              <v-btn class="ml-percent" color="white">Detalles</v-btn>
+            </div>
+          </v-col>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
     <v-card-actions>
       <v-btn small color="error" @click="deleteItem">Remove</v-btn>
     </v-card-actions>
@@ -123,13 +114,13 @@ export default {
   methods: {
     retrieveLessons() {
       LessonApiService.getAll()
-        .then(response => {
-          this.lessons = response.data
-          this.displayLessons = response.data.map(this.getDisplayLesson)
-        })
-        .catch((e) => {
-          console.log(e)
-        })
+          .then(response => {
+            this.lessons = response.data
+            this.displayLessons = response.data.map(this.getDisplayLesson)
+          })
+          .catch((e) => {
+            console.log(e)
+          })
     },
     getDisplayLesson(lesson) {
       var t
@@ -210,21 +201,21 @@ export default {
 
       }else{
         LessonApiService.create(this.editedIndex)
-          .then(response => {
-            let item = response.data
-            this.lessons.push(item)
-            this.displayLessons.push(this.getDisplayLesson(item))
-          })
+            .then(response => {
+              let item = response.data
+              this.lessons.push(item)
+              this.displayLessons.push(this.getDisplayLesson(item))
+            })
       }
     },
     deleteLesson(id) {
       LessonApiService.delete(id)
-        .then(() => {
-          this.refreshList()
-        })
-        .catch((e) => {
-          console.log(e)
-        })
+          .then(() => {
+            this.refreshList()
+          })
+          .catch((e) => {
+            console.log(e)
+          })
     }
 
   },
@@ -235,5 +226,22 @@ export default {
 </script>
 
 <style scoped>
-
+.m-list-item-title{
+  font-size: 17px;
+  font-weight:455;
+}
+.subtitle-1{
+  font-weight:555;
+}
+.content{
+  background-color: #F3F9FA;
+  border-radius: .3em;
+}
+.ml-percent{
+  margin-left: 25%;
+  min-width: 60px;
+  height: 30px !important;
+  color: #1282A2;
+  border: 1px solid #1282A2 !important;
+}
 </style>

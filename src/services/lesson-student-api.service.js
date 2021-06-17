e84514e3be282b17c4bb44203014bfb9bc7ebb04
Brigitte <u201914955@upc.edu.pe>
2021-06-17 01:47:58 -0500

    refactor: returns all data

--- src/services/lesson-student-api.service.js
+++ src/services/lesson-student-api.service.js
@@ -1,6 +1,8 @@
 import http from './http.common';
 class LessonStudentApiService{
-
+    getLessonStudentsByLessonId(lessonId){
+        return http.get(`/lessons/${lessonId}`)
+    }
 }
 
 export default new LessonStudentApiService();

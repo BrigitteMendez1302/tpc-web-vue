1b86c02f394e2eac4ab6a182b36ece5ec11301bf
Rodrigo Calle <rodrigocallegaldos@gmail.com>
2021-06-25 00:21:01 -0500

    add: view of q&a

--- src/views/questions-and-answers.vue
+++ src/views/questions-and-answers.vue
@@ -12,16 +12,14 @@
     <div class="answers-container" v-for="name in qandaArray" :key="name.id">
       <v-card class="answer-card elevation-1" shaped v-bind:id="name.id">
         <v-card-title class="title-answer white--text">{{name.title}}</v-card-title>
-        <v-card-text class="text-answer black--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias assumenda beatae consequatur,
-          deserunt ea earum, esse fugiat incidunt laboriosam molestias natus porro qui recusandae sit tempora totam vel veritatis?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto, aspernatur dolor eius eligendi iste labore modi molestias porro
-          repellendus sed similique tempora, temporibus? Eligendi labore pariatur ratione sint voluptates!
+        <v-card-text class="text-answer black--text">{{name.content}}
         </v-card-text>
-        <v-card-subtitle class="link black--text font-weight-medium">Go to page linked</v-card-subtitle>
+        <v-card-subtitle class="link black--text font-weight-medium">
+          <a href="https://brigittemendez1302.github.io/TPC-OpenSource/" target="_blank">
+            {{name.sub}}
+          </a>
+        </v-card-subtitle>
       </v-card>
-
-
-
 
     </div>
   </v-container>
@@ -32,22 +30,27 @@ export default {
   name: "questions-and-answers",
   data: function () {
     return {
-      qandaArray: [{id:1, title:'¿Qué es TPC?', link:'"#new"'},
-        {id:2, title:'¿Por dónde empiezo?' },
-        {id:3, title:'¿Quiénes pueden usar la plataforma?' },
-        {id:4, title:'Conoce los beneficios de TPC' },
-        {id:5, title:'¿El servicio esta dirigido solamente para universidades?' },
-        {id:6, title:'¿Cuáles son los medios de pago?' },
-        {id:7, title:'Quiero aportar ideas de mejora' },
-        {id:8, title:'¿Cómo modificar mi informacion personal?' },
-        {id:9, title:'¿Deseo cancelar mi plan TPC?' },
-        {id:10, title:'¿Buscas una guía practica sobre TPC?' } ]
+      qandaArray: [{id:1, title:'¿Qué es TPC?', content: 'TPC es una plataforma creada con el principal objetivo de generar una excelente experiencia de aprendizaje online, ' +
+            'a traves de la mejora de procesos de enseñanza, brindando una plataforma que permita la interacción de estudiantes y tutores en sus respectivas clases.', sub: 'Obtener más información' },
+        {id:2, title:'¿Por qué trabajar con TPC?', content: 'TPC se diferencia de las demas plataformas, debido a su alto nivel de calidad en cuanto al servicio ofrecido.' +
+              'TPC implementa nuevas funcionalidades que estan orientadas al manejo de la información.', sub: 'Obtener más información'},
+        {id:3, title:'Pilares de TPC', content: 'El servicio de TPC esta especializado para clases como talleres y tutorías.' +
+              'En base a la enseñanza especifica de estas clases, TPC garantiza cumplir con los pilares de rapidez, accesibilidad y simplicidad.', sub: 'Pilares de TPC'},
+        {id:4, title:'Conoce los beneficios de TPC', content: 'La plataforma brinda un servicio y experiencia de calidad, ' +
+              'TPC trabaja con datos reales, de la demanda y oferta de los cursos.' +
+              'TPC realiza calculos estadisticos de la eficiencia de los servicios de tutorias y talleres.' +
+              'TPC realiza seguimiento del desempeño de los tutores.', sub: 'Beneficios de TPC'},
+        {id:5, title:'¿Cuáles son los planes de TPC?', content: 'Nuestra plataforma de enseñanza virtual ofrece dos planes. El plan Basic gratuito permite' +
+              'la creación, actualización y seguimiento de las reservas. Acceso a videoconferencias, entre otras funcionalidades básicas.' +
+              'El Plan Pro Ilimitado incluye las funcionalidades básicas incluidas. Seguimiento del rendimiento de los docentes.' +
+              'Información estadistica relacionada a la materia, entre más funciones avanzadas.', sub: 'Visualizar los planes'}]
     }
   }
 }
 </script>
 
 <style scoped>
+
 .container {
   align-content: center;
   justify-content: center;
